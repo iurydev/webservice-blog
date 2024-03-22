71cd1ae14fa33f3a345553aec045378e83b2ec3a
Iury Rodrigues Castro <iurycastro@Iurys-MacBook-Air.local>
2024-03-22 10:21:30 -0300

    Initial commit

---
+++ src/test/java/com/spring/blogwebservice/BlogWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blogwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
