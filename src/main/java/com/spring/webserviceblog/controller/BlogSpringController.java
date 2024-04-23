16369ed118f12efc6f3a00c0877d31118b97bb45
iurydev <iurydev@gmail.com>
2024-04-22 21:44:02 -0300

    Feat: Adding method to return a post by ID.

--- src/main/java/com/spring/webserviceblog/controller/BlogSpringController.java
+++ src/main/java/com/spring/webserviceblog/controller/BlogSpringController.java
@@ -4,6 +4,7 @@ import com.spring.webserviceblog.model.PostModel;
 import com.spring.webserviceblog.service.BlogSpringService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,6 +21,14 @@ public class BlogSpringController {
         ModelAndView mv = new ModelAndView("posts");
         List<PostModel> posts = blogSpringService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        PostModel post = blogSpringService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 }
