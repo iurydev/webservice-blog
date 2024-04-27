96f8ffcf22663342236d9586544fa4e3e035e7f6
iurydev <iurydev@gmail.com>
2024-04-26 23:39:51 -0300

    Feat: Changing existing methods to return JSON data instead HTML templates with data.

--- src/main/java/com/spring/webserviceblog/controller/BlogSpringController.java
+++ src/main/java/com/spring/webserviceblog/controller/BlogSpringController.java
@@ -4,13 +4,14 @@ import com.spring.webserviceblog.model.PostModel;
 import com.spring.webserviceblog.service.BlogSpringService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -21,35 +22,29 @@ public class BlogSpringController {
     BlogSpringService blogSpringService;
 
     @RequestMapping(value = "/posts", method = RequestMethod.GET)
-    public ModelAndView getPosts() {
-        ModelAndView mv = new ModelAndView("posts");
+    public ResponseEntity<List<PostModel>> getPosts() {
         List<PostModel> posts = blogSpringService.findAll();
-        mv.addObject("posts", posts);
-        return mv;
-    }
-
-    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
-    public ModelAndView getPostDetails(@PathVariable("id") long id) {
-        ModelAndView mv = new ModelAndView("postDetails");
-        PostModel post = blogSpringService.findById(id);
-        mv.addObject("post", post);
-        return mv;
-    }
-
-    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
-    public String getPostForm() {
-        return "postForm";
-    }
-
-    @RequestMapping(value = "newpost", method = RequestMethod.POST)
-    public String savePost(@Valid PostModel post, BindingResult result, RedirectAttributes attributes) {
-        if (result.hasErrors()) {
-            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
-            return "redirect:/newpost";
+        if (posts.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
+        return ResponseEntity.ok(posts);
+    }
+
+    @RequestMapping(value = "/post/{id}", method = RequestMethod.GET)
+    public ResponseEntity<PostModel> getPostDetails(@PathVariable("id") long id) {
+        PostModel post = blogSpringService.findById(id);
+        if(post == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(post);
+    }
+
+
+    @RequestMapping(value = "newpost", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PostModel> savePost(@Valid @RequestBody PostModel post) {
         post.setData(LocalDate.now());
-        blogSpringService.save(post);
-        return "redirect:/posts";
+        PostModel newPost = blogSpringService.save(post);
+        return ResponseEntity.ok(newPost);
     }
 
 }
